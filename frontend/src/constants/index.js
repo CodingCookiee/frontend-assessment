d15dac5934107be7e43e1612fd27bb31c60af975
CodingCookiee <rzqutubawan@gmail.com>
2025-02-28 14:31:00 +0500

    update

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -31,45 +31,61 @@ export const features = [
   }
 ];
 
+export const popularNFTs = {
+  firstRow: [
+    { id: 'first1', image: '/images/popular/first-row-img-1.png' },
+    { id: 'first2', image: '/images/popular/first-row-image-2.png' }
+  ],
+  center: { id: 'center', image: '/images/popular/center.png' },
+  secondRow: [
+    { id: 'second1', image: '/images/popular/second-row-img-1.png' },
+    { id: 'second2', image: '/images/popular/second-row-image-2.png' }
+  ],
+  thirdRow: [
+    { id: 'third1', image: '/images/popular/third-row-img-1.png' },
+    { id: 'third2', image: '/images/popular/third-row-img-2.png' }
+  ]
+};
+
 export const navLinks = [
-    {
-      id: "home",
-      name: "Home",
-      href: "#home",
-    },
-    {
-      id: "about",
-      name: "About",
-      href: "#about",
-    },
-    {
-      id: "artist",
-      name: "Artist",
-      href: "#artist",
-    },
-    {
-      id: "staking",
-      name: "Staking",
-      href: "#staking",
-    },
-    {
-      id: "roadmap",
-      name: "Roadmap",
-      href: "#roadmap",
-    },
-    {
-      id: "team",
-      name: "Team",
-      href: "#team",
-    },
-    {
-      id: "vesting",
-      name: "Vesting",
-      href: "#vesting",
-    },
-    {
-      id: "marketplace",
-      name: "Marketplace",
-      href: "#marketplace",
-    },
-  ];
+  {
+    id: "home",
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: "about",
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: "artist",
+    name: "Artist",
+    href: "#artist",
+  },
+  {
+    id: "staking",
+    name: "Staking",
+    href: "#staking",
+  },
+  {
+    id: "roadmap",
+    name: "Roadmap",
+    href: "#roadmap",
+  },
+  {
+    id: "team",
+    name: "Team",
+    href: "#team",
+  },
+  {
+    id: "vesting",
+    name: "Vesting",
+    href: "#vesting",
+  },
+  {
+    id: "marketplace",
+    name: "Marketplace",
+    href: "#marketplace",
+  },
+];
